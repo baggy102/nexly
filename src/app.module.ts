f829e22442d7f243a1e4ba58290e2e90965b9cf4
baggy102 <rlgh0806@gmail.com>
2025-08-10 15:00:29 +0900

    feature/user module 작업

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './user/auth/auth.module';
+import { CompanyModule } from './company/company.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/nexly'),
     UserModule,
-    AuthModule],
+    AuthModule,
+    CompanyModule],
   controllers: [AppController],
   providers: [AppService],
 })
